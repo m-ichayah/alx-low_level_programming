e516dbf02a7c4edb3601e9887ecccdc8bbf242cd
m-ichayah <ini.michayah@gmail.com>
2022-09-12 21:40:49 -0700

    Program that prints add possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,15 +4,15 @@
  * main - Prints all possible combinations of two different digits, 
  * in ascending order, separated by a coma followed by a space.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int digit1, digit2;
 
-for (digit1 =0; digit1 < 9; digit1++)
+for (digit1 = 0; digit1 < 9; digit1++)
 {
-for (digit2 = digit1 +1; digit2 < 10; digit2++)
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
