46449dcac831100235133fddf1ab25cfd5be3a56
m-ichayah <ini.michayah@gmail.com>
2022-09-27 13:43:09 -0700

    function that fills memory with a constatnt byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,14 +10,14 @@
 char *_memset(char *s, char b, unsigned int n)
 
 {
-		unsigned int i;
+	unsigned int i;
 
-			/*Delacring FOR*/
-			for (i = 0; i < n; i++)
-					{
-								*(s + i) = b; /*add 1 position s*/
+	/*Delacring FOR*/
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b; /*add 1 position s*/
 
-									} /*END FOR*/
+	} /*END FOR*/
 
-				return (s);
+	return (s);
 }
